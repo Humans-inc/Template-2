d26e603b87f0bc78f96346342c6742a2725984dd
kot-vmeshke <81980041+kot-vmeshke@users.noreply.github.com>
2023-04-13 11:05:52 +0200

    Create new-bonus.js

---
+++ widgets/new-bonus/new-bonus.js
@@ -0,0 +1,20 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+function declOfNum(n, text_forms) {
+  n = Math.abs(n) % 100;
+  var n1 = n % 10;
+  if (n > 10 && n < 20) {
+    return text_forms[2];
+  }
+  if (n1 > 1 && n1 < 5) {
+    return text_forms[1];
+  }
+  if (n1 == 1) {
+    return text_forms[0];
+  }
+  return text_forms[2];
+}
+const textForms = ['рубль', 'рубля', 'рублей'];
+let userBalance = document.querySelector('#balance_virtual').innerText;
+document.querySelector('.new-bonus__count').innerHTML = `${userBalance || '0'}<div class="new-bonus__units">${declOfNum(+userBalance, textForms)}</div>`;
+})
