6138aaeb2f2055ee3e69a7c71404d89656d78e91
kot-vmeshke <81980041+kot-vmeshke@users.noreply.github.com>
2023-06-13 16:41:17 +0200

    Create lessons-with-bg.js

---
+++ scripts/lessons-with-bg.js
@@ -0,0 +1,74 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const lessonsGC = document.querySelectorAll(".lesson-list li:not(.divider)");
+  const lessonsCustom = document.querySelector("#new-lessons");
+  let dataLessons = Array.from(lessonsGC).map((item, index) => {
+    return {
+      title: item
+        .querySelector(".link.title")
+        .innerText.replace("просмотрено", ""),
+      number: index + 1,
+      itemImg: getComputedStyle(
+        item.querySelector(".item-image")
+      ).getPropertyValue("background-image"),
+      href: item
+        .querySelector("a")
+        .href.replace(`https://${window.location.hostname}`, ""),
+      state: item.classList[0],
+      descr: item.querySelector(".description")
+        ? item.querySelector(".description").innerText
+        : "",
+      date: item.querySelector(".has-start-at")
+        ? item
+            .querySelector(".has-start-at")
+            .innerText.replace("Недоступен до", "Откроется")
+        : "",
+    };
+  });
+  for (let lesson of dataLessons) {
+    switch (lesson.state) {
+      case "user-state-accomplished":
+        lessonStateText = "Принят";
+        break;
+      case "user-state-reached":
+        lessonStateText = "Доступен";
+        break;
+      case "user-state-need_accomplish":
+        lessonStateText = "Есть задание";
+        break;
+      case "user-state-answered":
+        lessonStateText = "На проверке";
+        break;
+      case "user-state-has_mission":
+        lessonStateText = "Есть задание";
+        break;
+      case "user-state-not_reached":
+        lessonStateText = "Закрыт";
+        break;
+    }
+    let newLesson = document.createElement("a");
+    newLesson.href = lesson.href;
+    newLesson.classList.add("new-lesson", lesson.state);
+    newLesson.innerHTML = `
+        <span class="new-lesson__number">Урок ${lesson.number}</span>
+        <span class="new-lesson__state">${lessonStateText}</span>
+        <span class="new-lesson__img"></span>
+        <span class="new-lesson__title">${lesson.title}</span>
+        <span class="new-lesson__descr">${lesson.descr}</span>
+        <span class="new-lesson__button">${
+          lesson.state === "user-state-not_reached"
+            ? "Недоступен"
+            : "Смотреть урок"
+        }</span>
+      `;
+    newLesson.style.background = `${lesson.itemImg} center center / cover no-repeat`;
+    if (lesson.state === "user-state-not_reached" && lesson.date.length) {
+      newLesson
+        .querySelector(".new-lesson__button")
+        .insertAdjacentHTML(
+          "beforeend",
+          `<span class="new-lesson__button-time">${lesson.date}</span>`
+        );
+    }
+    lessonsCustom.append(newLesson);
+  }
+});
